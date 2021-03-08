bfff733358a4906e811ca99ae784a87a9b7cfc55
ksheronov <skapank@mail.ru>
2021-03-08 12:17:55 +0300

    изменения

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class StudentInfo {
+    public static void main(String [] args){
+        System.out.println("Konstantin Sher");
+        System.out.println("01.01.2021");
+    }
+}
