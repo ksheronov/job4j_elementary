f071f7038eaecbe2a58394eeec68c4f15bfac4bc
ksheronov <skapank@mail.ru>
2021-03-20 15:26:26 +0300

    4.3.6. Math.abs [#486069]

--- src/ru/job4j/condition/ChessBoard.java
+++ src/ru/job4j/condition/ChessBoard.java
@@ -4,9 +4,8 @@ public class ChessBoard {
     public static int way(int x1, int y1, int x2, int y2) {
         if (Math.abs(x2 - x1) == Math.abs(y2 - y1)) {
             return Math.abs(x2 - x1);
-        } else {
+        }
             return 0;
         }
 
-    }
 }
