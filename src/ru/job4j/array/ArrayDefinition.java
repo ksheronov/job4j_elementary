81e7934c809332e211b3a486edd2d6cb33a813be
ksheronov <skapank@mail.ru>
2021-03-28 11:17:33 +0300

    6.0.2. Заполнение массива. [#486091]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива ages = " + ages.length);
         System.out.println("Размер массива surnames = " + surnames.length);
         System.out.println("Размер массива prices = " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Konstantin Sheronov";
+        names[1] = "Petp Petrov";
+        names[2] = "Ivan Ivanov";
+        names[3] = "Test Testov";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
