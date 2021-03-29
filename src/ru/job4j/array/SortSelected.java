3d539918de07d21101dab267a0f7452e6232bf94
ksheronov <skapank@mail.ru>
2021-03-29 23:59:02 +0300

    6.5.3. Сортировка выборкой. [#486085]

--- src/ru/job4j/array/SortSelected.java
+++ src/ru/job4j/array/SortSelected.java
@@ -1,8 +1,5 @@
 package ru.job4j.array;
 
-import ru.job4j.array.FindLoop;
-import ru.job4j.array.MinDiapason;
-
 public class SortSelected {
     public static int[] sort(int[] data) {
         for (int i = 0; i < data.length; i++) {
--- test/ru/job4j/array/SortSelectedTest.java
+++ test/ru/job4j/array/SortSelectedTest.java
@@ -2,7 +2,6 @@ package ru.job4j.array;
 
 import org.junit.Assert;
 import org.junit.Test;
-import ru.job4j.array.SortSelected;
 
 public class SortSelectedTest {
 
