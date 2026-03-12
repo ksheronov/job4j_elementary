4d14e6bdd97c6239e283c0ef39d3166a9f979001
ksheronov <skapank@mail.ru>
2026-03-12 09:39:39 +0300

    Тестирование Класса Калькулятора Температуры Хранения

--- src/main/java/ru/job4j/calculator/TemperatureFit.java
+++ src/main/java/ru/job4j/calculator/TemperatureFit.java
@@ -6,15 +6,7 @@ public class TemperatureFit {
     }
 
     public static double idealTemperatureForMeat(int temperature) {
-
         return temperature - 5.0;
     }
 
-    public static void main(String[] args) {
-        int temperature = 20;
-        double fruitsTemp = TemperatureFit.idealTemperatureForFruits(temperature);
-        double meatTemp = TemperatureFit.idealTemperatureForMeat(temperature);
-        System.out.println("Ideal temperature for fruits is " + fruitsTemp + "°C");
-        System.out.println("Ideal temperature for meat is " + meatTemp + "°C");
-    }
 }
---
+++ src/test/java/ru/job4j/calculator/TemperatureFitTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class TemperatureFitTest {
+
+    @Test
+    void whenRoomTemperature12ThenFruits10() {
+        int input = 12;
+        double expected = 10.0;
+        double output = TemperatureFit.idealTemperatureForFruits(input);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenRoomTemperature15ThenMeat10() {
+        int input = 15;
+        double expected = 10.0;
+        double output = TemperatureFit.idealTemperatureForMeat(input);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+}
