b0d86d572715bf36703eb37c9a5f7fb53b4d5135
ksheronov <skapank@mail.ru>
2026-03-14 15:02:58 +0300

    Поиск минимума в диапазоне [#505239]

---
+++ src/main/java/ru/job4j/array/SortSelected.java
@@ -0,0 +1,14 @@
+package ru.job4j.array;
+
+import static ru.job4j.array.SwitchArray.*;
+
+public class SortSelected {
+    public static int[] sort(int[] data) {
+        for (int i = 0; i < data.length - 1; i++) {
+            int min = MinDiapason.findMin(data, i, data.length - 1);
+            int index = FindLoop.indexInRange(data, min, i, data.length - 1);
+            swap(data, i, index);
+        }
+        return data;
+    }
+}
